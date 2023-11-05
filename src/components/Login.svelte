ab37b7655ab2cf7c3bb0095aab3ddcd0c47a627d
shashi <shashipanta57@gmail.com>
2023-11-05 21:28:33 +0545

    initial: Added easy-use-form for form and updated README.md

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -5,4 +5,7 @@ import { useForm, validators, HingGroup, Hint, email, required } from 'svelte-us
 <form use:form>
 <h1>Login</h1>
 
+<input type="email" name="email" use:validators={[required, email]} />
+
+
 </form>
