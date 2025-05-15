ba0100a71b2dcdb5af4a154125e8e41f880c9beb
barbosa <shashipanta57@gmail.com>
2025-05-15 15:17:26 +0545

    refactor: update generic toast component

--- src/components/toast/GenericToast.svelte
+++ src/components/toast/GenericToast.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
   import type { GlobalApiResponse } from "$lib/api";
   import { apiResponse } from "$lib/service/ApiService";
-    import { Toast } from "flowbite-svelte";
-    import { CheckCircleSolid, ExclamationCircleSolid, FireOutline, CloseCircleSolid } from 'flowbite-svelte-icons';
+  import { Toast } from "flowbite-svelte";
+  import {
+      CloseCircleSolid
+  } from "flowbite-svelte-icons";
 
-    interface ToastProp {
-      status: string,
-      message: string
-    }
+  interface ToastProp {
+    status: string;
+    message: string;
+  }
 
-    export let prop: GlobalApiResponse; 
+  export let prop: GlobalApiResponse;
 
-    console.log("Toast prop inside Toast: ", prop);
+  console.log("Toast prop inside Generic Toast: ", prop);
 </script>
 
 {#if $apiResponse}
-  <Toast color="red" >
-      <svelte:fragment slot="icon">
-        <CloseCircleSolid class="w-5 h-5" />
-        <span class="sr-only">Error icon</span>
-      </svelte:fragment>
-      {$apiResponse.message}
+  <Toast color="red">
+    <svelte:fragment slot="icon">
+      <CloseCircleSolid class="w-5 h-5" />
+      <span class="sr-only">Error icon</span>
+    </svelte:fragment>
+    {$apiResponse.message}
   </Toast>
-
 {/if}
